1ebaf24650bb33d10615ad838a566c4290d222e3
onursalha <onursalha22@gmail.com>
2023-04-02 22:18:11 -0500

    first commit

---
+++ src/DaisyFood/WetFood.java
@@ -0,0 +1,10 @@
+package DaisyFood;
+
+public class WetFood {
+    public static void main(String[] args) {
+        int wetFood= 10;
+        if(wetFood>5){
+            System.out.println("Daisy has enough wet food.");
+        }
+    }
+}
